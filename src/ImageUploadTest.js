7395ab441d7fd88ca37939858e6c4fc81e81992d
Lincoln Anderson <landerson@352inc.com>
2019-06-16 17:52:00 -0400

    add ImageUploadTest

---
+++ src/ImageUploadTest.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { compose, defaultProps, withHandlers } from 'recompose';
+
+const ImageUploadTest = compose(
+  defaultProps({
+    onFileChange: (fileUrl) => {
+      console.warn('Warning: missing "onFileChange" prop on ImageUploadTest component.')
+    },
+  }),
+  withHandlers({
+    handleFileInputChange: ({ onFileChange }) => (event) => {
+      const files = event.target.files;
+      const fileURL = window.URL.createObjectURL(files[0]);
+      onFileChange && onFileChange(fileURL);
+    },
+  }),
+)((props) => (
+  <input type="file" accept="image/*" onChange={props.handleFileInputChange} />
+));
+
+export default ImageUploadTest;
