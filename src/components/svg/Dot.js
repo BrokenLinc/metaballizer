a2459d8ae6968bf3d7d9b9b3f4cc62348c626165
Lincoln Anderson <landerson@352inc.com>
2019-06-16 23:14:45 -0400

    add subpixel killer

--- src/components/svg/Dot.js
+++ src/components/svg/Dot.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
+const SUBPIXEL_KILLER = 1.02;
+
 const Dot = ({cx, cy, scale, ...restProps}) => (
-  <circle cx={cx / scale} cy={cy / scale} r={0.5 / 1.44 / scale} {...restProps} />
+  <circle cx={cx / scale} cy={cy / scale} r={0.5 / 1.44 / scale * SUBPIXEL_KILLER} {...restProps} />
 );
 
 export default Dot;
