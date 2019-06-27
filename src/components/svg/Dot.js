e619ff13f4794a922353e77d1f1da55acedc598f
Lincoln Anderson <landerson@threefivetwo.com>
2019-06-26 23:54:01 -0400

    remove subpixel fix and flatten connector paths

--- src/components/svg/Dot.js
+++ src/components/svg/Dot.js
@@ -1,9 +1,7 @@
 import React from 'react';
 
-const SUBPIXEL_KILLER = 1.02;
-
 const Dot = ({cx, cy, scale, ...restProps}) => (
-  <circle cx={cx / scale} cy={cy / scale} r={0.5 / 1.44 / scale * SUBPIXEL_KILLER} {...restProps} />
+  <circle cx={cx / scale} cy={cy / scale} r={0.5 / 1.44 / scale} {...restProps} />
 );
 
 export default Dot;
