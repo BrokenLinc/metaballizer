458e30d9834fc62e06bcaf56f8ff7ba025c69ede
Lincoln Anderson <landerson@threefivetwo.com>
2019-06-27 00:38:29 -0400

    increase precision on dot radius

--- src/components/svg/Dot.js
+++ src/components/svg/Dot.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
+const sin45 = Math.cos(45 * Math.PI / 180) * 2;
+
 const Dot = ({cx, cy, scale, ...restProps}) => (
-  <circle cx={cx / scale} cy={cy / scale} r={0.5 / 1.44 / scale} {...restProps} />
+  <circle cx={cx / scale} cy={cy / scale} r={0.5 / sin45 / scale} {...restProps} />
 );
 
 export default Dot;
